69862ce7e3fbf20280d29e8db41296e14c184ffa
Tafadzwa Chiwaye <Tafadzwa@fairheads.com>
2022-08-08 15:18:23 +0200

    Added angular client

--- API/Startup.cs
+++ API/Startup.cs
@@ -28,6 +28,7 @@ namespace API
             });
 
             services.AddControllers();
+            services.AddCors();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
@@ -47,6 +48,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
